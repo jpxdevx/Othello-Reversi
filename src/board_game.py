adfadc7af5478db37a8cca5e1486e24a21070b5e
Koushik Sharma <1974kamalsharma@gmail.com>
2026-04-10 20:37:55 +0530

    redo push

--- src/board_game.py
+++ src/board_game.py
@@ -12,6 +12,8 @@ tile = grid / 8   #1 for 64 tiles
 line_thickness = 0.05
 tile_buttons = {}
 pieces = {}
+player_turn = True
+total_delay = 0
 
 def update():
     menu.update()
@@ -46,30 +48,34 @@ def board():
     def show_game_over():
         black_score = sum(row.count(1) for row in game_logic.board)
         white_score = sum(row.count(-1) for row in game_logic.board)
-
+        display = Button(disabled=True,position=(0,0,-1.5),scale=(0.3,0.2))
         if black_score > white_score:
-            result = f"You Win!  {black_score} - {white_score}"
+            result = f"You Win!\n{Entity(model="circle",parent=display,color=color.black, position=(-0.25,0,-2),scale=(0.2,0.2))}   {Entity(model="circle",parent=display,color=color.white,position=(0.25,0,-2),scale=(0.2,0.2))}\n{black_score} - {white_score}"
             col = color.green
         elif white_score > black_score:
-            result = f"AI Wins!  {black_score} - {white_score}"
+            result = f"AI Wins!\n{Entity(model="circle",parent=display,color=color.black,position=(-0.25,0,-2),scale=(0.2,0.2))}   {Entity(model = "circle",parent=display,color=color.white,position=(0.25,0,-2),scale=(0.2,0.2))}\n{black_score} - {white_score}"
             col = color.red
         else:
-            result = f"Tie!  {black_score} - {white_score}"
+            result = f"Tie!\n{Entity(model = "circle",parent=display,color=color.black,position=(-0.25,0,-2),scale=(0.2,0.2))}   {Entity(model = "circle",parent=display,color=color.white,position=(0.25,0,-2),scale=(0.2,0.2))}\n{black_score} - {white_score}"
             col = color.yellow
 
-        Button(disabled=True,position=(0,0,-3),scale=(4,4))
-        Text(result, parent=camera.ui, position=(0, 0,-4),origin=(0, 0), color=col, scale=2, font='VeraMono.ttf')
+        
+        Text(result, parent=camera.ui, position=(0, 0,-2),origin=(0, 0), color=col, scale=2, font='VeraMono.ttf')
 
     #AI to play
     def AI_turn():
+        global player_turn, total_delay
+        player_turn = False
         game_logic.fetch_token_valid_moves(-1,1)
         if not game_logic.valid:
             no_moves()
-            game_logic.fetch_token_valid_moves(-1,1)
+            game_logic.fetch_token_valid_moves(1,-1)
             if not game_logic.valid:
                 # player has no moves either
                 game_logic.game_over = True
                 show_game_over()
+            else:
+                player_turn = True
             return
         
         old_board = [r[:] for r in game_logic.board]
@@ -83,15 +89,27 @@ def board():
             ux,uy = to_3d_coords(r,c)
             place_piece(ux,uy,color.gray)
             flip_pieces(old_board,(ux,uy))
-           
+        #i want to add a delay before the player's turn
+        player_turn = True
+        #checking for players move 
         game_logic.fetch_token_valid_moves(1, -1)
         if not game_logic.valid:
-            # player has no moves either
-            game_logic.game_over = True
-            show_game_over()
+            no_moves()
+            #AI has no moves either
+            game_logic.fetch_token_valid_moves(-1, 1)
+            if not game_logic.valid:
+                game_logic.game_over = True
+                show_game_over()
+            else:
+                player_turn = False
+                invoke(AI_turn,delay=0.5)
 
     def handle_player_move(x, y):
 
+        global player_turn, total_delay
+
+        if game_logic.game_over or player_turn == False:
+            return
         #handle the players move
         row,col = to_board_coords(x, y)
         old_board = [row[:] for row in game_logic.board]
@@ -100,9 +118,23 @@ def board():
         if not move_made:
             invalid()
             return
-        
+        player_turn = False
         place_piece(x, y, color.black)
         animation_dur = flip_pieces(old_board, (x, y))
+
+        game_logic.fetch_token_valid_moves(-1, 1)
+
+        if not game_logic.valid:
+            no_moves()
+
+            #check if player also has no moves
+            game_logic.fetch_token_valid_moves(1, -1)
+
+            if not game_logic.valid:
+                game_logic.game_over = True
+                show_game_over()
+            return
+        
         #adding a 0.8 sec pause for the Ai to start
         total_delay = animation_dur + 0.8
         invoke(AI_turn,delay = total_delay)
@@ -151,9 +183,9 @@ def board():
         #top half and bottom half - swap colors based on starting color
         if starting_color == color.black:
             top_col = color.black
-            bot_col = color.gray
+            bot_col = color.rgb(230,230,230)
         else:
-            top_col = color.gray
+            top_col = color.rgb(230,230,230)
             bot_col = color.black
 
         top = Entity(parent=p, model=Cylinder(resolution=64), scale=(1, 0.5, 1), color=top_col,y=-0.25)
@@ -177,7 +209,7 @@ def board():
     right  = Entity(parent = board_container,model='cube', scale=(border, grid, 1),position=( offset, 0, 0),texture="textures/board_texture.jpg", texture_scale=(border, grid))
 
     # Main board
-    Entity(parent = board_container,model='cube', scale=(grid, grid, 0.2), position=(0, 0, 0), color=rgb(0, 128, 0))
+    Entity(parent = board_container,model='cube', scale=(grid, grid, 0.2), position=(0, 0, 0), color="#1F6040")
     thick_lines = 0
     # Vertical and horizontal lines
     for i in range(-4, 5):
